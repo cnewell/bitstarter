f32f524492d6bfcdeead8d3329db5e8c500dbc35
Chris Newell <cnewell@wetpaint.com>
2013-07-18 19:04:38 +0000

    Updating web.js to dump index.html instead of hardcoded string

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var index_buffer = fs.readFileSync("index.html");
+  response.send(index_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
