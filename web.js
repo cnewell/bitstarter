246ec80609521f4d8a616b9284a1e8bae0d1e8a7
Chris Newell <cnewell@wetpaint.com>
2013-07-22 23:56:57 +0000

    Initial population with project-specific content.

--- web.js
+++ web.js
@@ -8,6 +8,8 @@ app.get('/', function(request, response) {
   response.send(index_buffer.toString());
 });
 
+app.use(express.static(__dirname + '/public'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
